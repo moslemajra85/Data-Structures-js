2834e31b05b063ba7cd49b3383a0f347644bedb9
Moslem Ajra <ajra.engineering@gmail.Com>
2023-10-17 09:54:45 +0100

    Implement Queue data structure using javascript array

--- queue/queue.js
+++ queue/queue.js
@@ -1,3 +1,20 @@
-class Queue {}
+/**
+ * Implement Class Queue that will serve as a blueprint
+ * for Queue datastructur.
+ */
 
-module.exports = Queue
+class Queue {
+  constructor() {
+    this.data = []; // this is what will hold data
+  }
+
+  add(item) {
+    this.data.unshift(item);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
+
+module.exports = Queue;
